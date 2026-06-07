aeb1aed5cb57270bb72c81f6a85d0d74e9d396c4
vedika82 <anandvedika420@gmail.com>
2026-06-07 17:09:46 +0530

    file style.css added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require ("express");
 const app = express();
 const server = require ("http").Server(app);
+// for using ejs
 app.set('view engine','ejs')
+// for using all the static files inside of public folder
+app.set(express.static('public'))
 
 app.get('/',(req,res)=>{
     res.render('room')
