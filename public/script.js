0bb786a98a2ea4e87fb82a6683e289877f5eb19e
vedika82 <anandvedika420@gmail.com>
2026-06-23 20:36:13 +0530

    invite button

--- public/script.js
+++ public/script.js
@@ -69,16 +69,55 @@ const addVideoStream = (video, stream) => {
   });
 };
 
-// const inviteButton = document.querySelector("#inviteButton");
-// const muteButton = document.querySelector("#muteButton");
-// const stopVideo = document.querySelector("#stopVideo");
+const inviteButton = document.querySelector("#inviteButton");
+const muteButton = document.querySelector("#muteButton");
+const stopVideo = document.querySelector("#stopVideo");
+const disconnectBtn = document.querySelector("#disconnect");
 
-// muteButton.addEventListener("click", () => {
-//   const enabled = myVideoStream.getAudioTracks()[0].enabled;
-//   if (enabled) {
-//     myVideoStream.getAudioTracks()[0].enabled = false;
-//     muteButton.innerHTML = "Unmute";
-//   } else {
-//     myVideoStream.getAudioTracks()[0].enabled = true;
-//     muteButton.innerHTML = "Mute";
-//   }
+muteButton.addEventListener("click",() => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    html = `<i class="fas fa-microphone-slash"></i>`;
+    muteButton.classList.toggle("background_red");
+    muteButton.innerHTML = html;
+  }
+  else{
+    myVideoStream.getAudioTracks()[0].enabled = true;
+    html = `<i class="fas fa-microphone"></i>`;
+    muteButton.classList.toggle("background_red");
+    muteButton.innerHTML = html;
+  }
+})
+
+stopVideo.addEventListener("click",() => {
+  const enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    html = `<i class="fas fa-video-slash"></i>`;
+    stopVideo.classList.toggle("background_red");
+    stopVideo.innerHTML = html;
+  }
+  else{
+    myVideoStream.getVideoTracks()[0].enabled = true;
+    html = `<i class="fas fa-video"></i>`;
+    stopVideo.classList.toggle("background_red");
+    stopVideo.innerHTML = html;
+  }
+})
+
+inviteButton.addEventListener("click",() => {
+  prompt("Copy this link and send it to people you want to have video call with",
+  window.location.href
+  );
+})
+
+disconnectBtn.addEventListener("click",() => {
+  peer.destroy();
+  const myVideoElement = document.querySelector("video");
+  if(myVideoElement){
+    myVideoElement.remove();
+  }
+  socket.emit("disconnect");
+  window.location.href = "https://www.google.com";
+})
